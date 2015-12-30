001f176ceb9641d6e3630ef2842f03cccb8b1526
Pi-works <pi-works@Pi-worksui-MacBook-Pro.local>
2015-12-30 16:51:07 +0900

    pipayment controller fliter  modify++

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -41,8 +41,8 @@ class PaymentController extends Controller
             $user = $this->sentry->getUser();
             $user_id  = $user->id;
             
-            if( isset($request['fliter'] ) ) {
-                $invoices = Invoice::where('user_id','=',$user_id)->where('status','=',$request['fliter'] )->orderBy( 'id' , 'desc' )->paginate($pagePer);
+            if( isset($request['filter'] ) ) {
+                $invoices = Invoice::where('user_id','=',$user_id)->where('status','=',$request['filter'] )->orderBy( 'id' , 'desc' )->paginate($pagePer);
             } else {
                 $invoices = Invoice::where('user_id','=',$user_id)->orderBy( 'id' , 'desc' )->paginate($pagePer);
             }
