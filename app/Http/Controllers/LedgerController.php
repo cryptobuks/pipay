0d80086e61bee2b9d88716854370b4392c0093b1
Pi-works <pi-works@Pi-worksui-MacBook-Pro.local>
2016-01-04 12:34:29 +0900

    LedgerController added index data

--- app/Http/Controllers/LedgerController.php
+++ app/Http/Controllers/LedgerController.php
@@ -38,22 +38,40 @@ class LedgerController extends Controller
         $user = $this->sentry->getUser();
         $user_id  = $user->id;
 
-        $transactions = Transaction::where('user_id', '=', $user_id)->orderBy( 'id' , 'desc' )->paginate(15);
-        $transactions->load('account');
-        
-        $jsonTable = [];
-            
-        foreach ( $transactions as $transaction){
-            $jsonTable[] = array(
-                'id' => $transaction->id
-            );
-        }
-
         if ($request->ajax()) {
+            $transactions = Transaction::where('user_id', '=', $user_id)->orderBy( 'id' , 'desc' )->paginate(15);
+            
+            $jsonTable = [];
+                
+            foreach ( $transactions as $transaction){
+                $jsonTable[] = array(
+                    'id' => $transaction->id,
+                    'amount' => $transaction->amount,
+                    'fee' => $transaction->fee,
+                    'net' => $transaction->net,
+                    'status' => $transaction->status,
+                    'type' => $transaction->type,
+                    'created_at' => $transaction->created_at
+                );
+            }
+
             return $jsonTable;
         }
 
-        return view('ledgers.index', compact('jsonTable'));
+        $AccountJson = [];
+
+        $accounts = Account::where('user_id', '=', $user_id)->get();
+        
+        foreach ( $accounts as $account) {
+            if( 'PI' == $account->currency ){
+                $AccountJson[] = array( 'PI' => $account->balance - $account->locked );
+            } 
+            else if ( 'KRW' == $account->currency) {
+                $AccountJson[] = array( 'KRW' => $account->balance - $account->locked );
+            }
+        }        
+
+        return view('ledgers.index', compact('AccountJson'));
     }
 
 }
