c9b9c455a92971b28693a47ff4aa7626eddb85c3
Pi-works <pi-works@Pi-worksui-MacBook-Pro.local>
2016-01-06 10:59:54 +0900

    RefundController store function modify+

--- app/Http/Controllers/RefundController.php
+++ app/Http/Controllers/RefundController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Cartalyst\Sentry\Sentry;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
